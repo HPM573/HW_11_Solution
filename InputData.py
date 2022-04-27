7809aedd2ef716fbb761f6b9951a6da04c7ac7ed
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2022-04-26 20:59:55 -0400

    2022 correcting treatment cost

--- InputData.py
+++ InputData.py
@@ -7,7 +7,7 @@ ALPHA = 0.05
 DISCOUNT = 0.03     # annual discount rate
 
 P_STROKE = 0.05         # annual probability of stroke in state Well
-P_RE_STROKE = 0.2     # annual probability of recurrent stroke
+P_RE_STROKE = 0.1     # annual probability of recurrent stroke
 P_SURV = 0.7       # probability of surviving a stroke
 
 
@@ -35,7 +35,7 @@ ANNUAL_STATE_COST = [
     5000,   # STROKE
 ]
 
-ANTICOAG_COST = 2000
+ANTICOAG_COST = 3000
 
 # anticoagulation relative risk in reducing stroke incidence while in “Post-Stroke”
-ANTICOAG_RR = 0.55
+ANTICOAG_RR = 0.45
